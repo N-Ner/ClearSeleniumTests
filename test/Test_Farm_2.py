b6bb6eb1beb668000ff58fbca7e7d6d67b47c7eb
Nazar Ner <n.ner@scalr.com>
2017-03-06 08:27:03 +0200

    Test_Farm_2 - searchrole by field

---
+++ test/Test_Farm_2.py
@@ -0,0 +1,19 @@
+__author__ = 'Nazar Ner'
+
+def test_scalr_farm(app):
+
+    containerid = '8104b4ffeb57'
+    farmid = '9800012'
+    farmrole = 'base-ubuntu1404'
+    #Uncomment the line for Scope settings
+    #scope = "img[data-qtip='This Role is defined in the Scalr Scope']"
+    #scope = "img[data-qtip='TThis Role is defined in the Account Scope']"
+    scope = "img[data-qtip='This Role is defined in the Environment Scope']"
+
+    app.farm.go_to_container(containerid)
+    app.farm.login_user_accaunt()
+    app.farm.goto_list()
+    app.farm.search(containerid, farmid)
+    app.farm.search_by_field(farmrole, scope)
+    app.farm.add_role()
+    app.farm.launch(containerid, farmid)
