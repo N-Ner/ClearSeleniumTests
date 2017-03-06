311a5e03e7846262101d1b5306995e9e67f04fde
Nazar Ner <n.ner@scalr.com>
2017-03-06 08:20:23 +0200

    Update farm.py

--- farm/farm.py
+++ farm/farm.py
@@ -17,15 +17,29 @@ class Farm:
 
     def add_role(self):
         driver = self.app.driver
-        driver.find_element_by_xpath("//a[.='Add farm role']").click()
-        time.sleep(2)
-        driver.find_element_by_xpath("//*[.='Quick start']").click()
-        driver.find_element_by_xpath("//*[.='MySQL']").click()
-        time.sleep(3)
         driver.find_element_by_xpath("//a[.='Add to farm']").click()
         time.sleep(1)
         driver.find_element_by_xpath("//a[.='Save farm']").click()
         time.sleep(1)
+    
+    def search_by_field(self, farmrole, scope):
+        driver = self.app.driver
+        driver.find_element_by_xpath("//a[.='Add farm role']").click()
+        time.sleep(3)
+        driver.find_element_by_xpath("//*[.='Base']").click()
+        driver.find_element_by_css_selector("div.x-form-text-wrap-focus> input").clear()
+        driver.find_element_by_css_selector("div.x-form-text-wrap-focus> input").send_keys(farmrole)
+        time.sleep(2)
+        driver.find_element_by_css_selector(scope).click()
+        time.sleep(2)
+        
+    def search_quick_start(self):
+        driver = self.app.driver
+        driver.find_element_by_xpath("//a[.='Add farm role']").click()
+        time.sleep(2)
+        driver.find_element_by_xpath("//*[.='Quick start']").click()
+        driver.find_element_by_xpath("//*[.='MySQL']").click()
+        time.sleep(3)       
 
     def search(self, containerid, farmid):
         driver = self.app.driver
