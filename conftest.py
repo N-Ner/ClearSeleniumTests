a32874c7a0ba4c107e1347007195cdeca045987a
Nazar Ner <n.ner@scalr.com>
2017-03-22 14:40:23 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,9 +3,9 @@ __author__ = 'Nazar Ner'
 import pytest
 from fixture.application import Application
 
+
 @pytest.fixture(scope = "session")
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
-
